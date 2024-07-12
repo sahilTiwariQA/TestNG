783c6a9d9dff45388454cf9dad8b886519cf244a
Sahil Tiwari <sahiltiwari649@gmail.com>
2024-07-13 00:48:24 +0530

    Adding all files to GIT from first TestNG project

---
+++ src/test/java/org/example/day1.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+
+    @Test
+    //This is a TestNG annotation which considers below code as test. So even when you don't write public static void main, below method can be run via TestNG instead of Java.
+    public void Demo()
+    {
+        //TestNG needs all test to be in a method.
+        System.out.println("Hello world");
+    }
+    @Test
+    public void Demo2()
+    {
+        System.out.println("Bye");
+    }
+}
+
+/*
+Interview questions:
+1. How to run tests with TestNG?
+Ans: You need to have @Test annotation followed by methods.
+
+
+
+
+
+ */
